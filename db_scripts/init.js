6d03f8144ac206279ad9878335a89a6b966ee63d
Petterluring <petter.gustafsson1998@gmail.com>
2026-05-18 13:54:14 +0200

    Added a docker service that initializes the database server with a new database containing two collections. The service executes the db_scripts/init.js to initialize it

--- db_scripts/init.js
+++ db_scripts/init.js
@@ -1,7 +1,45 @@
-const dbName = "split_expenses";
+const dbName = process.env.SPLIT_EXPENSE_DB;
+const splitExpenseUser = process.env.SPLIT_EXPENSE_DB_USER
+const splitExpensePassword = process.env.SPLIT_EXPENSE_DB_PASSWORD
+
+if (!(dbName && splitExpenseUser && splitExpensePassword)) {
+    throw new Error('Environment variables SPLIT_EXPENSE_DB, SPLIT_EXPENSE_USER, and SPLIT_EXPENSE_PASSWORD must be configured')
+}
+
 
 const result = db.adminCommand({ listDatabases: 1 });
 
-const exists = result.databases.some(database => database.name === dbName);
+const exists = result.databases.some(({ name }) => name === dbName);
 
 print(`${dbName} exists: ${exists}`);
+
+if (!exists) {
+    print(`Creating ${dbName} with collections "users" and "groups...`)
+    const newDb = db.getSiblingDB("split_expenses");
+
+    newDb.createCollection("users")
+    newDb.createCollection("groups")
+}
+
+const adminDb = db.getSiblingDB("admin")
+const userInfo = adminDb.runCommand({usersInfo: 1})
+
+
+const splitExpenseUserExists = userInfo.users.some(
+    ({ user, db }) => user === splitExpenseUser && db === "admin"
+);
+
+print(`${splitExpenseUser} exists: ${splitExpenseUserExists}`);
+
+if (!splitExpenseUserExists) {
+    print(`Creating user: ${splitExpenseUser}`)
+    adminDb.createUser({
+            user: splitExpenseUser,
+            pwd: splitExpensePassword,
+            roles: [{role: "readWrite", db: dbName}]
+        }
+    )
+}
+
+
+
