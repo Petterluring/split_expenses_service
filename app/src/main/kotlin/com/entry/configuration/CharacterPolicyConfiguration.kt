9914bd6c784d5b1656f04545cb67f08775e1e1b1
Petterluring <petter.gustafsson1998@gmail.com>
2026-05-25 15:57:16 +0200

    Added username and password validation for user creation request

--- app/src/main/kotlin/com/entry/configuration/CharacterPolicyConfiguration.kt
+++ app/src/main/kotlin/com/entry/configuration/CharacterPolicyConfiguration.kt
@@ -7,4 +7,23 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class CharacterPolicyConfiguration {
 
+    @Bean
+    fun passwordPolicy(): CharacterPolicy = CharacterPolicy(
+        minLength = 8,
+        policyName = "Password policy",
+        includeLowercase = true,
+        includeUppercase = true,
+        includeNumbers = true,
+        includeSymbols = true
+    )
+
+    @Bean
+    fun usernamePolicy(): CharacterPolicy = CharacterPolicy(
+        minLength = 4,
+        policyName = "Username policy",
+        includeLowercase = true,
+        includeUppercase = false,
+        includeNumbers = true,
+        includeSymbols = false
+    )
 }
--- app/src/main/kotlin/com/entry/service/UserService.kt
+++ app/src/main/kotlin/com/entry/service/UserService.kt
@@ -1,15 +1,29 @@
 package com.entry.service
 
+import com.entry.characterpolicy.CharacterPolicy
 import com.entry.dto.generic.MessageResponseDto
 import com.entry.dto.user.CreateUserRequestDto
 import com.entry.exception.InvalidRequestException
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 
 @Service
-class UserService {
+class UserService(
+    @param:Qualifier("passwordPolicy")
+    private val passwordPolicy: CharacterPolicy,
+    @param:Qualifier("usernamePolicy")
+    private val usernamePolicy: CharacterPolicy,
+) {
     fun create(user: CreateUserRequestDto): MessageResponseDto {
         if (user.username.isBlank() || user.password.isBlank()) {
             throw InvalidRequestException("Username or password is missing")
+        }
+
+        if (passwordPolicy.matches(user.password) || usernamePolicy.matches(user.username)) {
+            val policies = "${passwordPolicy.toString()}\n${usernamePolicy.toString()}"
+            throw InvalidRequestException(
+                "Username or password is invalid. These must comply with the following policies:\n${policies}",
+            )
         }
 
         return MessageResponseDto(
