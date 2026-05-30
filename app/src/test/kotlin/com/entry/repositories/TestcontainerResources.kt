66e83462c5a07c4afe05f3678c9a11c222d86a8a
Petterluring <petter.gustafsson1998@gmail.com>
2026-05-30 13:05:29 +0200

    Was finally able to integrate repository tests properly.

--- app/src/test/kotlin/com/entry/repositories/TestcontainerResources.kt
+++ app/src/test/kotlin/com/entry/repositories/TestcontainerResources.kt
@@ -6,24 +6,32 @@ import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.mongodb.MongoDBContainer
 import org.testcontainers.utility.DockerImageName
 
-abstract class TestcontainerResources {
+@Testcontainers
+interface TestcontainerResources {
     companion object {
-        val mongoDbContainer: MongoDBContainer = MongoDBContainer(DockerImageName.parse("mongo:7.0"))
-            .withReuse(true)
-
+        val mongoDbContainer: MongoDBContainer =
+            MongoDBContainer(DockerImageName.parse("mongo:7.0"))
+                .withReuse(true)
 
         @JvmStatic
         @DynamicPropertySource
         fun mongoProperties(registry: DynamicPropertyRegistry) {
             println("DynamicPropertySource was called")
-            registry.add("spring.data.mongodb.uri") {
-                mongoDbContainer.replicaSetUrl
+            registry.add("spring.mongodb.host") {
+                mongoDbContainer.host
+            }
+            registry.add("spring.mongodb.database") {
+                "test"
+            }
+            registry.add("spring.mongodb.port") {
+                mongoDbContainer.getMappedPort(27017)
             }
         }
 
         init {
             mongoDbContainer.start()
             println("Mongo URI = ${mongoDbContainer.replicaSetUrl}")
+            println("Mongo Port = ${mongoDbContainer.getMappedPort(27017)}")
         }
     }
 }
--- app/src/test/kotlin/com/entry/repositories/UserRepositoryTest.kt
+++ app/src/test/kotlin/com/entry/repositories/UserRepositoryTest.kt
@@ -1,47 +1,34 @@
 package com.entry.repositories
 
-/*
 import com.entry.model.user.User
 import com.entry.repository.UserRepository
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.data.mongodb.test.autoconfigure.DataMongoTest;
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.core.env.Environment
-import org.springframework.test.context.ActiveProfiles
-import org.testcontainers.junit.jupiter.Testcontainers
+import org.springframework.boot.data.mongodb.test.autoconfigure.DataMongoTest
 import kotlin.test.assertTrue
 
-
 @DataMongoTest
-class UserRepositoryTest : TestcontainerResources() {
-
+class UserRepositoryTest : TestcontainerResources {
     @Autowired
     lateinit var userRepository: UserRepository
 
-    @Autowired
-    lateinit var environment: Environment
+    @Test
+    fun test() {
+    }
 
-    /*
     @BeforeEach
     fun setup() {
         userRepository.deleteAll()
     }
-    */
 
-    @Test
-    fun test() {
-        println("Spring sees Mongo URI = ${environment.getProperty("spring.data.mongodb.uri")}")
-    }
-
-    /*
     @Test
     fun `can check if user exists`() {
-        val user = User(
-            username = "cat",
-            hashedPassword = "cat_password"
-        )
+        val user =
+            User(
+                username = "cat",
+                hashedPassword = "cat_password",
+            )
 
         userRepository.save(user)
 
@@ -49,7 +36,4 @@ class UserRepositoryTest : TestcontainerResources() {
 
         assertTrue { exists }
     }
-    */
-
 }
-*/
